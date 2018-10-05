ff7fd26fb4ee9f1e242dcf6ff64f462986e8f07e
fernando <jc.fernando03@gmail.com>
2018-10-05 18:25:11 -0400

    primer modelo de la pagina

---
+++ ConferenceSystem/app/Post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $dates = ['published_at'];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+}
---
+++ ConferenceSystem/database/seeds/PostsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+use App\Post;
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $post = new Post;
+        $post->title = "Mi primer post";
+        $post->excerpt = "Extracto de mi primer post";
+        $post->body = "<p>Contenido de mi primer post</p>";
+        $post->published_at = Carbon::now();
+        $post->category_id = 1;
+        $post->save();
+    }
+}
